53d0e070ceaeeb4bd9e6ef24cca52acdad199f85
wailovet <wailovet@163.com>
2023-01-15 20:37:44 +0800

    'fix'

--- cluster.go
+++ cluster.go
@@ -1,100 +1,27 @@
 package goincv
 
 import (
-	"encoding/json"
-	"math"
+	"github.com/mpraski/clusters"
 )
 
 type KMeansCluster struct {
-	data      [][]float32
-	centroids [][]float32
+	data [][]float64
+	c    clusters.HardClusterer
 }
 
 func (k *KMeansCluster) Add(data []float32) {
-	k.data = append(k.data, data)
+	k.data = append(k.data, F32ToF64(data))
 }
 
-func (k *KMeansCluster) Learn(clusters, iterations int) {
-	// Initialize centroids randomly
-	for i := 0; i < clusters; i++ {
-		centroid := k.data[i]
-		k.centroids = append(k.centroids, centroid)
+func (k *KMeansCluster) Learn(clasNum, iterations int) (err error) {
+	k.c, err = clusters.KMeans(iterations, clasNum, clusters.EuclideanDistance)
+	if err != nil {
+		return err
 	}
-
-	for i := 0; i < iterations; i++ {
-		// Assign points to nearest centroid
-		clusters := make([][][]float32, clusters)
-
-		// find nearest centroid for each point
-		for _, point := range k.data {
-			nearestCentroid := 0
-			minDistance := float32(math.MaxFloat32)
-			for j, centroid := range k.centroids {
-				distance := euclideanDistance(point, centroid)
-				if distance < minDistance {
-					minDistance = distance
-					nearestCentroid = j
-				}
-			}
-			clusters[nearestCentroid] = append(clusters[nearestCentroid], point)
-		}
-
-		// Recalculate centroids
-		for j := range k.centroids {
-			newCentroid := calculateCentroid(clusters[j])
-			k.centroids[j] = newCentroid
-		}
-	}
+	err = k.c.Learn(k.data)
+	return err
 }
 
 func (k *KMeansCluster) Predict(data []float32) int {
-	nearestCentroid := 0
-	minDistance := float32(math.MaxFloat32)
-	for i, centroid := range k.centroids {
-		distance := euclideanDistance(data, centroid)
-		if distance < minDistance {
-			minDistance = distance
-			nearestCentroid = i
-		}
-	}
-	return nearestCentroid
-}
-
-func (k *KMeansCluster) Load(filename string) {
-	// Code to load model from file
-
-	json.Unmarshal(ReadFileContent(filename), &k.centroids)
-}
-
-func (k *KMeansCluster) Save(filename string) {
-	JsonToFile(filename, k.centroids)
-}
-
-func euclideanDistance(a, b []float32) float32 {
-	var distance float32
-	for i := range a {
-		distance += (a[i] - b[i]) * (a[i] - b[i])
-	}
-	return float32(math.Sqrt(float64(distance)))
-}
-
-func calculateCentroid(points [][]float32) []float32 {
-	var centroid []float32
-	if len(points) > 0 {
-		// initialize centroid with zeroes
-		centroid = make([]float32, len(points[0]))
-
-		// Sum up all the points
-		for _, point := range points {
-			for j := range point {
-				centroid[j] += point[j]
-			}
-		}
-
-		// Divide by number of points to get the average
-		for i := range centroid {
-			centroid[i] /= float32(len(points))
-		}
-	}
-	return centroid
+	return k.c.Predict(F32ToF64(data))
 }
